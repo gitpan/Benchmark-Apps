f71a199faff9a780fb91b2e44df3e3d8b3936ce2
Alberto Manuel Brandão Simões <ambs@perl-hackers.net>
2008-08-30 20:23:35 +0000

    Import of AMBS/Benchmark-Apps-0.03 from CPAN.
    gitpan-cpan-distribution: Benchmark-Apps
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         AMBS/Benchmark/Benchmark-Apps-0.03.tar.gz
    gitpan-cpan-author:       AMBS
    gitpan-cpan-maturity:     released

--- lib/Benchmark/Apps.pm
+++ lib/Benchmark/Apps.pm
@@ -11,7 +11,7 @@ Benchmark::Apps - Simple interface to benchmark applications.
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
@@ -19,39 +19,126 @@ our $VERSION = '0.02';
 This module provides a simple interface to benchmark applications (not
 necessarily Perl applications).
 
-    use Benchmark::Apps;
+  use Benchmark::Apps;
 
-		Benchmark::Apps::compare_these(
-			cmd1 => 'run_command_1 with arguments',
-			cmd2 => 'run_command_2 with other arguments',
-		);
+  my $b = Benchmark::Apps->new(
+                             pretty_print=>1,
+                             iters=>5,
+                           );
+
+  my %result = $b->run(
+                     cmd1 => 'run_command_1 with arguments',
+                     cmd2 => 'run_command_2 with other arguments',
+                   );
 
 =head1 FUNCTIONS
 
-=head2 compare_these
+=head2 new
 
-This is the main method of the module. You call it and it prints nice
-formated results of the benchmarks performed. The first argument might
-be a reference to an hash. In that case, the hash is used as
-configuration information.
+Create a new benchmarking object with a set of options. The set of
+options is a simple hash. Available options at the moment are:
+
+=over 4
+
+=item C<pretty_print>
+
+When enabled it will print to stdout, in a formatted way the results
+of the benchmarks as they finish running. This option should de used
+when you want to run benchmarks and want to see the results progress
+as the tests run. You can disable it, so you can perform automated
+benchmarks.
+
+Options: true (1) or false (0)
+
+Default: false (0)
+
+=item C<iters>
+
+This is the number of iterations that each test will run.
+
+Options: integer greater than 1
+
+Default: 5
+
+=item C<args>
+
+This is a reference to an anonymous function that will calculate the
+command argument based on the iteraction number.
+
+Options: any function reference that returns a string
+
+Default: identity function: the iteration number is passed as an argument
+to the command)
+
+=back
+
+=head2 run
+
+This method runs the commands described in the hash passed as argument.
+It returns an hash of the results and return codes for each command.
 
 =cut
 
-sub compare_these {
-	my $cfg = { iters => 5 };
-	$cfg = { %{shift @_}, %$cfg} if ref $_[0] eq "HASH";
-	my %command = @_;
+sub _id { return shift }
 
-	_show_start_info($cfg, %command);
-	
-	for my $iter (1..$cfg->{iters}) {
-		_show_iter($iter);
+my %cfg = ( pretty_print => 1,
+	          iters        => 5 ,
+	 					args         => \&_id );
+my %command = ();
+my %res = ();
 
-		for my $c (keys %command) {
-			my $time = time_this($command{$c});
-			printf " %8s => %8.4f s\n", $c, $time;
+sub new {
+	my $class = shift;
+	my %args = @_;
+	my $self = bless({}, $class);
+
+   my @options = keys %cfg;
+
+	foreach (@options) {
+		if (defined $args{$_} and _validate_option($_,$args{$_})) {
+			$cfg{$_} = $args{$_};
 		}
+	}
 
+	return $self;
+}
+
+sub _validate_option {
+	my ($option, $value) = @_;
+
+	# TODO do some validations
+	# validate everything ok for now
+
+	return 1;
+}
+
+sub run {
+	my $self = shift;
+	%command = @_;
+
+	for my $iter (1..$cfg{'iters'}) {
+		for my $c (keys %command) {
+			$res{$c}{'run'} = $command{$c};
+			my $time = time_this($command{$c} . ' ' . &{$cfg{'args'}}($iter));
+			$res{$c}{'result'}{$iter} = $time;
+		}
+	}
+	pretty_print(%res) if $cfg{'pretty_print'};
+
+	return %res;
+}
+
+sub pretty_print {
+	my $self = shift;
+
+	for my $c (sort keys %command) {
+   	for my $iter (1..$cfg{'iters'}) {
+      	_show_iter($iter);
+
+      	for my $c (keys %command) {
+         	printf " %8s => %8.4f s\n", $c, $res{$c}{'result'}{$iter};
+      	}
+		}
 	}
 }
 
@@ -66,7 +153,7 @@ of registering the elapsed time.
 sub time_this {
 	my $cmd_line = shift;
 	my $start_time = [gettimeofday];
-	system("$cmd_line > /dev/null");
+	system("$cmd_line 2>&1 > /dev/null");
 	return tv_interval($start_time);
 }
 
@@ -75,20 +162,63 @@ sub _show_iter {
 	printf "%d%s iteration:\n", $i, $i==1?"st":$i==2?"nd":$i==3?"rd":"th";
 }
 
-sub _show_start_info {
-	my ($cfg, %command) = @_;
-	
-	print "-" x 50, "\n";
-	printf "Running %d iterations of:\n", $cfg->{iters};
-	for my $c (keys %command) {
-		printf " %8s => %s\n", $c, $command{$c};
+=head1 EXAMPLES
+
+=head2 Example One
+
+This simple example illustrates how to run benchmarks.
+
+The commands hash tells the module which script to run, and options.
+
+  my %commands = (
+    cmd1=> 'script.pl --options',
+    cmd1=> 'script.py -options',
+  );
+
+The C<args> method is used to calculate the arguments that will be
+concatenated to the command line for each iteration. In this example
+we are saying that we want to pass as an argument to the command the
+current iteration times 1000.
+
+  sub args {
+		my $iteration = shift;
+		return $iteration * 1000;
 	}
-	print "-" x 50, "\n\n";
-}
+
+We then create our Benchmark::Apps instance, with pretty printing
+enabled. We also want to run each command 10 times and finally
+pass it an C<args> sub ref.
+
+  my $t = Benchmark::Apps->new( pretty_print => 1,
+	                              iters        => 10,
+	                              args         => \&args);
+
+Finally we run the benchmark for the commands defined.
+
+  $t->run(%command);
+
+This would be similiar to run the following commands, for the first
+iteration:
+
+  $ script.pl --options 10000
+  $ script.py --options 10000
+
+the second iteration:
+
+  $ script.pl --options 20000
+  $ script.py --options 20000
+
+the third iteration:
+
+  $ script.pl --options 30000
+  $ script.py --options 30000
+
+And so on... The result of each iteration will be printed to stdout.
 
 =head1 AUTHOR
 
-Aberto Simoes, C<< <ambs at cpan.org> >>
+Aberto Simoes (aka ambs), C<< <ambs at cpan.org> >>
+Nuno Carvalho (aka smash), C<< <smash @ cpan.org> >>
 
 =head1 BUGS
 
@@ -126,7 +256,7 @@ L<http://search.cpan.org/dist/Benchmark-Apps>
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2008 Aberto Simoes, all rights reserved.
+Copyright 2008 Aberto Simoes, Nuno Carvalho, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
