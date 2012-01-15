d4491822605431e1cb9dfd4ae533fa9f958cc24e
Alberto Manuel Brandão Simões <ambs@perl-hackers.net>
2012-01-15 17:30:06 +0000

    Import of AMBS/Benchmark-Apps-0.04 from CPAN.
    gitpan-cpan-distribution: Benchmark-Apps
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         AMBS/Benchmark/Benchmark-Apps-0.04.tar.gz
    gitpan-cpan-author:       AMBS
    gitpan-cpan-maturity:     released

--- lib/Benchmark/Apps.pm
+++ lib/Benchmark/Apps.pm
@@ -11,7 +11,7 @@ Benchmark::Apps - Simple interface to benchmark applications.
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 
 =head1 SYNOPSIS
@@ -21,22 +21,31 @@ necessarily Perl applications).
 
   use Benchmark::Apps;
 
-  my $b = Benchmark::Apps->new(
-                             pretty_print=>1,
-                             iters=>5,
-                           );
+  my $commands = {
+       cmd1 => 'run_command_1 with arguments',
+       cmd2 => 'run_command_2 with other arguments',
+     };
 
-  my %result = $b->run(
-                     cmd1 => 'run_command_1 with arguments',
-                     cmd2 => 'run_command_2 with other arguments',
-                   );
+  my $conf = { pretty_print=>1, iters=>5 };
+
+  Benchmark:Apps::run( $commands, $conf );
+
+=head1 DESCRIPTION
+
+This module can be used to perform simple benchmarks on programs. Basically,
+it can be used to benchmark any program that can be called with a system
+call.
 
 =head1 FUNCTIONS
 
-=head2 new
+=head2 run
 
-Create a new benchmarking object with a set of options. The set of
-options is a simple hash. Available options at the moment are:
+This method is used to run benchmarks. It runs the commands described in 
+the hash passed as argument. It returns an hash of the results each command.
+A second hash reference can be passed to this method: a configuration
+hash reference. The values passed in this hash override the default
+behaviour of the run method. The configuration options available at this
+moment are:
 
 =over 4
 
@@ -67,8 +76,8 @@ command argument based on the iteraction number.
 
 Options: any function reference that returns a string
 
-Default: identity function: the iteration number is passed as an argument
-to the command)
+Default: empty function: always returns an empty string, which means no
+arguments will be given to the command
 
 =back
 
@@ -79,67 +88,78 @@ It returns an hash of the results and return codes for each command.
 
 =cut
 
-sub _id { return shift }
+sub _empty { '' }
 
 my %cfg = ( pretty_print => 1,
-	          iters        => 5 ,
-	 					args         => \&_id );
+            iters        => 5 ,
+            args         => \&_empty );
 my %command = ();
 my %res = ();
 
-sub new {
-	my $class = shift;
-	my %args = @_;
-	my $self = bless({}, $class);
+sub run {
+	my @args = @_;
 
-   my @options = keys %cfg;
+	@args == 0 and die 'At least one hash reference needs to be passed as argument';
+	@args > 2 and die 'A maximum of two arguments (hash refs) should be passed to this function';
+	# in case we got the second argument (configuration hash ref)
+	if (@args > 1) {
+            if (ref $args[1] eq 'HASH') {
+   		my @l = keys %{$args[1]};
+   		foreach (@l) {
+                    if (defined $args[1]{$_}) { # XXX and validate args
+         		$cfg{$_} = $args[1]{$_};
+                    }
+   		}
+            }
+            else { warn 'Second argument to run should be an hash ref'; }
+	}
 
-	foreach (@options) {
-		if (defined $args{$_} and _validate_option($_,$args{$_})) {
-			$cfg{$_} = $args{$_};
+	%command = %{$args[0]};
+
+	for my $iter (1..$cfg{'iters'}) {
+		for my $c (keys %command) {
+			$res{$c}{'run'} = $command{$c};
+			my $time = time_this($command{$c}.' '.&{$cfg{'args'}}($iter));
+			$res{$c}{'result'}{$iter} = $time;
 		}
 	}
 
-	return $self;
+	pretty_print(%res) if $cfg{'pretty_print'};
+
+	return +{%res};
 }
 
 sub _validate_option {
 	my ($option, $value) = @_;
 
 	# TODO do some validations
-	# validate everything ok for now
+	# everything ok for now
 
 	return 1;
 }
 
-sub run {
-	my $self = shift;
-	%command = @_;
+=head2 pretty_print
 
-	for my $iter (1..$cfg{'iters'}) {
-		for my $c (keys %command) {
-			$res{$c}{'run'} = $command{$c};
-			my $time = time_this($command{$c} . ' ' . &{$cfg{'args'}}($iter));
-			$res{$c}{'result'}{$iter} = $time;
-		}
-	}
-	pretty_print(%res) if $cfg{'pretty_print'};
+This method is used to print the final result to STDOUT before returning 
+from the C<run> method.
 
-	return %res;
-}
+=cut
 
 sub pretty_print {
 	my $self = shift;
 
-	for my $c (sort keys %command) {
-   	for my $iter (1..$cfg{'iters'}) {
-      	_show_iter($iter);
+  	for my $iter (1..$cfg{'iters'}) {
+     	_show_iter($iter);
 
-      	for my $c (keys %command) {
-         	printf " %8s => %8.4f s\n", $c, $res{$c}{'result'}{$iter};
-      	}
-		}
+     	for my $c (keys %command) {
+        	printf " %8s => %8.4f s\n", $c, $res{$c}{'result'}{$iter};
+     	}
 	}
+}
+
+sub _show_iter {
+	my $i = shift;
+	printf "%d%s iteration:\n", $i, $i==1?"st":$i==2?"nd":$i==3?"rd":"th";
 }
 
 =head2 time_this
@@ -157,63 +177,10 @@ sub time_this {
 	return tv_interval($start_time);
 }
 
-sub _show_iter {
-	my $i = shift;
-	printf "%d%s iteration:\n", $i, $i==1?"st":$i==2?"nd":$i==3?"rd":"th";
-}
 
 =head1 EXAMPLES
 
-=head2 Example One
-
-This simple example illustrates how to run benchmarks.
-
-The commands hash tells the module which script to run, and options.
-
-  my %commands = (
-    cmd1=> 'script.pl --options',
-    cmd1=> 'script.py -options',
-  );
-
-The C<args> method is used to calculate the arguments that will be
-concatenated to the command line for each iteration. In this example
-we are saying that we want to pass as an argument to the command the
-current iteration times 1000.
-
-  sub args {
-		my $iteration = shift;
-		return $iteration * 1000;
-	}
-
-We then create our Benchmark::Apps instance, with pretty printing
-enabled. We also want to run each command 10 times and finally
-pass it an C<args> sub ref.
-
-  my $t = Benchmark::Apps->new( pretty_print => 1,
-	                              iters        => 10,
-	                              args         => \&args);
-
-Finally we run the benchmark for the commands defined.
-
-  $t->run(%command);
-
-This would be similiar to run the following commands, for the first
-iteration:
-
-  $ script.pl --options 10000
-  $ script.py --options 10000
-
-the second iteration:
-
-  $ script.pl --options 20000
-  $ script.py --options 20000
-
-the third iteration:
-
-  $ script.pl --options 30000
-  $ script.py --options 30000
-
-And so on... The result of each iteration will be printed to stdout.
+Check files in C<examples/>.
 
 =head1 AUTHOR
 
